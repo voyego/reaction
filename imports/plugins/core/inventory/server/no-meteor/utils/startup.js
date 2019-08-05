d73155520b72cf224178ea340ebf0390c2c56a32
Jaka Maver <Jaka.Maver@comtrade.com>
2019-08-05 10:35:16 +0200

    feat: modify logic how we decide isLowQuantity

--- imports/plugins/core/inventory/server/no-meteor/utils/startup.js
+++ imports/plugins/core/inventory/server/no-meteor/utils/startup.js
@@ -1,3 +1,4 @@
+import Logger from "@reactioncommerce/logger";
 /**
  * @summary Called on startup
  * @param {Object} context Startup context
@@ -5,7 +6,10 @@
  * @returns {undefined}
  */
 export default function startup(context) {
-  const { appEvents } = context;
+  const { appEvents, collections } = context;
+  const { Catalog, Products } = collections;
+  const { PRODUCT_LOW_INVENTORY_THRESHOLD } = process.env;
+  const DEFAULT_LOW_INVENTORY_THRESHOLD = 10;
 
   // Whenever inventory is updated for any sellable variant, the plugin that did the update is
   // expected to emit `afterInventoryUpdate`. We listen for this and keep the boolean fields
@@ -26,5 +30,53 @@ export default function startup(context) {
     uniqueProductIds.forEach((productId) => {
       context.mutations.partialProductPublish(context, { productId, startFrom: "inventory" });
     });
+/* TODO voyego: move this to partialProductPublish
+    const variants = await Products.find(
+      {
+        ancestors: productId,
+        isDeleted: { $ne: true },
+        isVisible: true
+      },
+      {
+        _id: 1,
+        ancestors: 1,
+        shopId: 1
+      }
+    ).toArray();
+
+    const topVariants = variants.filter((variant) => variant.ancestors.length === 1);
+    if (topVariants.length === 0) return;
+
+    const topVariantsInventoryInfo = await context.queries.inventoryForProductConfigurations(context, {
+      fields: ["isBackorder", "isLowQuantity", "isSoldOut"],
+      productConfigurations: topVariants.map((option) => ({
+        isSellable: !variants.some((variant) => variant.ancestors.includes(option._id)),
+        productId: option.ancestors[0],
+        productVariantId: option._id
+      })),
+      shopId: topVariants[0].shopId
+    });
+
+    const productQuantity = topVariantsInventoryInfo.reduce(
+      (sum, { inventoryInfo: { inventoryAvailableToSell } }) => sum + inventoryAvailableToSell,
+      0
+    );
+
+    if (!PRODUCT_LOW_INVENTORY_THRESHOLD) {
+      Logger.warn("Missing .env variable PRODUCT_LOW_INVENTORY_THRESHOLD, using default value.");
+    }
+
+    await Catalog.updateOne(
+      { "product.productId": productId },
+      {
+        $set: {
+          "product.isBackorder": topVariantsInventoryInfo.every(({ inventoryInfo }) => inventoryInfo.isBackorder),
+          "product.isLowQuantity":
+            productQuantity > (PRODUCT_LOW_INVENTORY_THRESHOLD || DEFAULT_LOW_INVENTORY_THRESHOLD),
+          "product.isSoldOut": topVariantsInventoryInfo.every(({ inventoryInfo }) => inventoryInfo.isSoldOut)
+        }
+      }
+    );
+    */
   });
 }
